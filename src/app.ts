694ae242a9852490355dd8e0d48b0101d2c55ea5
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2023-11-25 20:47:39 +0300

    default argument

--- src/app.ts
+++ src/app.ts
@@ -42,8 +42,20 @@ const printOutput: (output: number | string) => void = (output) =>
 
 const btn = document.querySelector("button");
 
-if(btn){
-    btn.addEventListener("click", event =>{console.log(event)});
+if (btn) {
+  btn.addEventListener("click", (event) => {
+    console.log(event);
+  });
 }
 
 printOutput(add(12, 34));
+
+//default argument
+//Arrow functions
+const addWithDefault = (a: number = 10, b: number) => a + b;
+printOutput(addWithDefault(undefined, 55));
+
+//do this instead
+const addWithDefault2 = (a: number, b: number=19) => a + b;
+printOutput(addWithDefault2(10));
+
