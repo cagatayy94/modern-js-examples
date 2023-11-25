f8c4eaa5322806c25a89be23fb5ddd5c91ee3854
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2023-11-25 20:26:21 +0300

    differences with let and var

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-// Code goes here!
+const userName = "Cagatay";
+//userName = "nasıl";
+
+let age = 30;
+
+age = 30;
+
+//bad works sometimes
+function add(a:number, b:number){
+    let result; // or var result;
+    result =  a+b;
+    return result;
+}
+
+//console.log(result);
+
+// doesnt have access variable 
+if(age > 20){
+    let isOld = false;
+}
+
+console.log(isOld);
+
+// can access variable but can have some memory issues
+if(age > 20){
+    var isOld = false;
+}
+
+console.log(isOld);
