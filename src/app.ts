536043a4f31bfb646353ef991365b2a46d76a243
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2024-01-07 22:32:03 +0300

    Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,95 +1,16 @@
-const userName = "Cagatay";
-//userName = "nasıl";
+interface Person{
+  name: string;
+  age: number;
 
-//let age = 30;
-
-//age = 30;
-/* 
-//bad works sometimes
-function add(a:number, b:number){
-    let result; // or var result;
-    result =  a+b;
-    return result;
+  greet(phrase: string): void;
 }
 
-//console.log(result);
+let user1: Person;
 
-// doesnt have access variable 
-if(age > 20){
-    let isOld = false;
+user1 = {
+  name: "cagatay",
+  age: 30,
+  greet(phrase: string){
+    console.log(phrase + " " + this.name)
+  }
 }
-
-console.log(isOld);
-
-// can access variable but can have some memory issues
-if(age > 20){
-    var isOld = false;
-}
-
-console.log(isOld); */
-
-//Arrow functions
-/* const add = (a: number, b: number) => a + b;
- */
-/* const printOutput = (output:string|number) => {
-    console.log(output);
-} */
-
-/* //shorter version of above
-const printOutput: (output: number | string) => void = (output) =>
-  console.log(output);
-// () => {}
-
-const btn = document.querySelector("button");
-
-if (btn) {
-  btn.addEventListener("click", (event) => {
-    console.log(event);
-  });
-}
-
-printOutput(add(12, 34)); */
-
-//default argument
-//Arrow functions
-/* const addWithDefault = (a: number = 10, b: number) => a + b;
-printOutput(addWithDefault(undefined, 55)); */
-
-//do this instead
-/* const addWithDefault2 = (a: number, b: number = 19) => a + b;
-printOutput(addWithDefault2(10)); */
-
-//The spread operator
-const hobbies = ["sports", "cookie"];
-const hobbiesOutdoor = ["biking", "climbing"];
-const activeHobbies = ["hiking", ...hobbiesOutdoor];
-
-activeHobbies.push(...hobbies);
-
-const person = {
-  username: "cagatay",
-  age: 23,
-};
-
-//copies only reference
-const copiedPersonReference = person;
-
-const copiedNewPerson = { ...person };
-
-//rest parameters
-const add = (...numbers: number[]) => {
-  return numbers.reduce((currentResult, currentValue) => {
-    return currentResult + currentValue;
-  }, 0);
-};
-
-const addNumbers = add(5, 10, 15, 20, 25);
-
-console.log(addNumbers);
-
-//array object destructuring
-const [hobby1, hobby2, ...remainingHobbies] = hobbies;
-console.log(hobbies, hobby1, hobby2);
-
-//object destructuring
-const { username, age } = person;
