c1c308217accc593233cd25c2fccca4e48367061
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2024-01-20 00:09:24 +0300

    intersection types

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-alert("asdas")
+type Admin = {
+  name: string;
+  privilages: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type ElevatedEmployee = Admin & Employee;
+
+const e1 : ElevatedEmployee = {
+    name:"Max",
+    privilages:["asd", "sda "],
+    startDate: new Date()
+}
