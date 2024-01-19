acf9deccc1f4701d88d9435132a45cdd76a13425
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2024-01-20 00:49:40 +0300

    Discriminated Unions

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,95 @@ type Employee = {
 
 type ElevatedEmployee = Admin & Employee;
 
-const e1 : ElevatedEmployee = {
-    name:"Max",
-    privilages:["asd", "sda "],
-    startDate: new Date()
+const e1: ElevatedEmployee = {
+  name: "Max",
+  privilages: ["asd", "sda "],
+  startDate: new Date(),
+};
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable | Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+
+  return a + b;
 }
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+
+  if ("privilages" in emp) {
+    console.log("Privilages: " + emp.privilages);
+  }
+
+  if ("startDate" in emp) {
+    console.log("StartDate: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation(e1);
+printEmployeeInformation({ name: "manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("driving");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("driving a truck");
+  }
+  loadCargo(amount: number) {
+    console.log("loading a cargo" + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if ("loadCargo" in vehicle || vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed: number;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+
+  console.log("mooving with speed" + animal);
+}
+
+moveAnimal({ type: "bird", flyingSpeed: 123 });
